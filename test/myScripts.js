2dd13ac209219b7ea02da4324df9dd3777f3515d
RobertWeckel <55281097+RobertWeckel@users.noreply.github.com>
2020-01-22 08:16:06 -0500

    Update myScripts.js

--- test/myScripts.js
+++ test/myScripts.js
@@ -1,4 +1,8 @@
-for(var x = 0; x <= 8; x++){
-  var y = Math.pow(2,x);
-  document.write(y + "<br />")
+function myLoop(){
+  var end=
+  document.getElementById("endLoop").value;
+for( var i = 0; i <= end; i++)
+{
+  document.write('<p>' + i + '</p>')
+}
 }
